f9f437f3c869ea268dc6cbab1810225e2265ecc8
ksenia-gezalova <ksenia.gezalova@gmail.com>
2018-09-06 17:04:08 +0300

    finish with first part of the task

--- js/goods.js
+++ js/goods.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var GOODS_AMOUNT = 26;
-var CONTENTS_AMOUNT = 5;
 var GOOD_NAMES = [
   'Чесночные сливки',
   'Огуречный педант',
@@ -31,6 +30,38 @@ var GOOD_NAMES = [
   'Невинные винные',
   'Бельгийское пенное',
   'Острый язычок'];
+
+var PICTURE_ROUTES = [
+  'gum-cedar.jpg',
+  'gum-chile.jpg',
+  'gum-eggplant.jpg',
+  'gum-mustard.jpg',
+  'gum-portwine.jpg',
+  'gum-wasabi.jpg',
+  'ice-cucumber.jpg',
+  'ice-eggplant.jpg',
+  'ice-garlic.jpg',
+  'ice-italian.jpg',
+  'ice-mushroom.jpg',
+  'ice-pig.jpg',
+  'marmalade-beer.jpg',
+  'marmalade-caviar.jpg',
+  'marmalade-corn.jpg',
+  'marmalade-new-year.jpg',
+  'marmalade-sour.jpg',
+  'marshmallow-bacon.jpg',
+  'marshmallow-beer.jpg',
+  'marshmallow-shrimp.jpg',
+  'marshmallow-spicy.jpg',
+  'marshmallow-wine.jpg',
+  'soda-bacon.jpg',
+  'soda-celery.jpg',
+  'soda-cob.jpg',
+  'soda-garlic.jpg',
+  'soda-peanut-grapes.jpg',
+  'soda-russian.jpg'
+];
+
 var GOOD_INGRIDIENTS = [
   'молоко',
   'сливки',
@@ -51,33 +82,29 @@ var GOOD_INGRIDIENTS = [
   'вилларибо',
   'виллабаджо'
 ];
-var amount = [];
-var price;
-var weight;
-var rating = {
-  value: [],
-  number: []
-};
-var nutrition_facts = {
-  sugar: true,
-  energy: [],
-  contents: []
-};
+
+var GOODS_CONTENT = 4;
+
+var MIN_VALUE_AMOUNT = 0;
+var MAX_VALUE_AMOUNT = 20;
+
+var MIN_VALUE_PRICE = 100;
+var MAX_VALUE_PRICE = 1500;
+
+var MIN_VALUE_WEIGHT = 30;
+var MAX_VALUE_WEIGHT = 300;
+
+var MIN_RATING_VALUE = 1;
+var MAX_RATING_VALUE = 5;
+
+var MIN_RATING_NUMBER = 10;
+var MAX_RATING_NUMBER = 900;
+
+var MIN_ENERGY_VALUE = 70;
+var MAX_ENERGY_VALUE = 500;
+
 var goods = [];
 
-
-
-var getNumbersRange = function (arr, minValue, maxValue) {
-  for (var i = minValue; i < maxValue + 1; i++) {
-    arr.push(i);
-  }
-  return arr;
-};
-
-getNumbersRange(amount, 0, 20);
-getNumbersRange(rating.value, 1, 5);
-getNumbersRange(rating.number, 10, 900);
-getNumbersRange(nutrition_facts.energy, 70, 500);
 
 var getRandomElement = function (arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
@@ -85,18 +112,62 @@ var getRandomElement = function (arr) {
   return randomElement;
 };
 
-var generateRandomContents = function (contents) {
-  for (var i = 0; i < CONTENTS_AMOUNT.length; i++) {
-    var randomData = getRandomElement(contents);
-    var randomContent = randomContent + randomData;
-    console.log(randomContent);
-  }
-  return randomContent;
+var getRandomValue = function (min, max) {
+  var random = min + Math.random() * (max + 1 - min);
+  random = Math.floor(random);
+  return random;
 };
 
+var getRandomContent = function (contents) {
+  var content = getRandomElement(contents);
+  for (var i = 0; i < GOODS_CONTENT; i++) {
+    content = getRandomElement(contents) + ', ' + content;
+  }
+  return content;
+};
+
+var getRandomBoolean = function () {
+  return getRandomValue(0, 1) === 1 ? true : false;
+};
+
+for (var j = 0; j < GOODS_AMOUNT; j++) {
+  goods.push({'name': GOOD_NAMES[j], 'picture': 'img/cards/' + getRandomElement(PICTURE_ROUTES), 'amount': getRandomValue(MIN_VALUE_AMOUNT, MAX_VALUE_AMOUNT), 'price': getRandomValue(MIN_VALUE_PRICE, MAX_VALUE_PRICE), 'weight': getRandomValue(MIN_VALUE_WEIGHT, MAX_VALUE_WEIGHT), 'rating': {'value': getRandomValue(MIN_RATING_VALUE, MAX_RATING_VALUE), 'number': getRandomValue(MIN_RATING_NUMBER, MAX_RATING_NUMBER)}, 'nutritionFacts': {'sugar': getRandomBoolean(), 'energy': getRandomValue(MIN_ENERGY_VALUE, MAX_ENERGY_VALUE), 'contents': getRandomContent(GOOD_INGRIDIENTS)}});
+}
+
+var userCatalog = document.querySelector('.catalog__cards');
+userCatalog.classList.remove('catalog__cards--load');
+var catalogLoad = document.querySelector('.catalog__load');
+catalogLoad.classList.add('visually-hidden');
+
+var cardTemplate = document.querySelector('#card').content.querySelector('.catalog__card');
+
 for (var i = 0; i < GOODS_AMOUNT; i++) {
-  goods.push({'name': GOOD_NAMES[i], 'amount': 20, 'price': '', 'weight': '', 'rating': '', 'nutrition_facts': ''});
+  var cardElement = cardTemplate.cloneNode(true);
+  userCatalog.appendChild(cardElement);
+
+  cardElement.querySelector('.card__title').textContent = GOOD_NAMES[i];
+
+  cardElement.querySelector('.star__count').textContent = goods[i].rating.number;
+  var stars = document.querySelector('.stars__rating');
+  if (goods[i].rating.value === 1) {
+    stars.classList.add('stars__rating--one');
+  } else if (goods[i].rating.value === 2) {
+    stars.classList.add('stars__rating--two');
+  } else if (goods[i].rating.value === 3) {
+    stars.classList.add('stars__rating--three');
+  } else if (goods[i].rating.value === 4) {
+    stars.classList.add('stars__rating--four');
+  } else {
+    stars.classList.add('stars__rating--five');
+  }
+
+  if (goods.amount > 5) {
+    cardElement.classList.add('card--in-stock');
+  } else if (goods.amount > 0 && goods.amount < 6) {
+    cardElement.classList.add('card--little');
+  } else {
+    cardElement.classList.add('card--soon');
+  }
 }
 
 console.log(goods);
-console.log(generateRandomContents(GOOD_INGRIDIENTS));
