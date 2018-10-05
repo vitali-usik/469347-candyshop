c4049da44c421ad51e838ecc5a3d894a3c4b48bf
ksenia-gezalova <ksenia.gezalova@gmail.com>
2018-10-05 14:17:10 +0300

    remobe disabled function

--- js/filters.js
+++ js/filters.js
@@ -10,8 +10,8 @@
   var rangeMax = range.querySelector('.range__price--max');
   var rangeMin = range.querySelector('.range__price--min');
 
-  // var itemsCount = document.querySelectorAll('.input-btn__item-count');
-  // var itemsLabel = document.querySelectorAll('.input-btn__label');
+  var itemsCount = document.querySelectorAll('.input-btn__item-count');
+  var itemsLabel = sidebar.querySelectorAll('.input-btn__label');
 
   var catalogCards = document.querySelector('.catalog__cards');
   // var catalog = document.querySelector('.catalog__cards-wrap');
@@ -196,19 +196,20 @@
     }
   };
 
-  /* var initCountKind = function (items) {
+  var initCountKind = function (items) {
     items.forEach(function (item, i) {
-      console.log(item);
-      console.log(Object.keys(window.data.types));
-       if (items[i].innerText === window.data.types[i]) {
-        item[i].textContent = window.data.types[i];
-      }
+      var currentType = items[i].innerText;
+      /*  console.log(currentType);
+      console.log(window.data.types['Мороженое']); */
+      /*  if (window.data.types[items[i].innerText]) {
+        items[i].textContent = window.data.types[items[i].innerText];
+      } */
     });
-  }; */
+  };
 
-  // console.log(window.data.types);
+  console.log(window.data.nutritionFacts['Без сахара']);
 
-  // initCountKind(itemsLabel);
+  initCountKind(itemsLabel);
 
 
   pinMove(rangeRight);
